d32662207f0b2840d5ea9ac23ba7de0c77897d29
Gerardo <the.z0w@gmail.com>
2025-03-25 10:50:30 -0600

    navbar

--- includes/header.php
+++ includes/header.php
@@ -7,11 +7,56 @@
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Inventario</a>
-        <div>
-            <a class="btn btn-outline-light" href="logout.php">Cerrar sesión</a>
+        <a class="navbar-brand" href="dashboard.php">Inventario</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContenido" aria-controls="navbarContenido" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarContenido">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <!-- Categoría: Inventario -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="menuInventario" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Inventario
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="menuInventario">
+                        <li><a class="dropdown-item" href="#">Ver Inventario</a></li>
+                        <li><a class="dropdown-item" href="#">Agregar Material</a></li>
+                        <li><a class="dropdown-item" href="#">Conteos</a></li>
+                    </ul>
+                </li>
+
+                <!-- Categoría: Reportes -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="menuReportes" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Reportes
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="menuReportes">
+                        <li><a class="dropdown-item" href="#">Movimientos</a></li>
+                        <li><a class="dropdown-item" href="#">Exportar Excel</a></li>
+                    </ul>
+                </li>
+
+                <!-- Categoría: Administración -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="menuAdmin" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Administración
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="menuAdmin">
+                        <li><a class="dropdown-item" href="crear_usuario.php">Crear Usuario</a></li>
+                        <li><a class="dropdown-item" href="#">Permisos</a></li>
+                        <li><a class="dropdown-item" href="#">Configuración</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="btn btn-outline-light btn-sm" href="logout.php">Cerrar sesión</a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
